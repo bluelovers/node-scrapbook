0d0cc7c226a067212ac3b51569f996144b677960
bluelovers <codelovers@users.sourceforge.net>
2016-03-24 05:50:04 +0800

    init (Step: 2)
    Step: 2
    
    [A ] new file:
    
    	README.md
    	app.js
    	gw2tp.njsproj
    	index.js
    	node_modules/.gitkeep
    	package.json

---
+++ gw2tp/app.js
@@ -0,0 +1,25 @@
+﻿console.log('Hello world');
+
+module.exports = function (window, $) 
+{ 
+
+}
+
+var $ = jQuery;
+
+console.log(jQuery.fn.jquery);
+
+console.log($.fn.jquery);
+
+
+$("body").append('<div class="testing">Hello World, It works</div>');
+
+
+
+console.log($(".testing").text());
+
+
+$.get("https://api.jquery.com/jQuery.get/", function (data) {
+    $(".testing").html(data);
+    console.log($(".testing").text());
+});
